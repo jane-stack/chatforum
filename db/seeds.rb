12a23b9c29f10f5982d9a9c8cb0cba17fd7a6dde
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-19 15:48:07 -0600

    Backend is working properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Topic.destroy_all
+
+# Users
+jane = User.create(username: "janeur", password: "Test1234#")
+jimmy = User.create(username: "jvng", password: "Test1234#")
+
+# Topic
+topic1 = Topic.create(name: "Ruby on Rails", description: "What are your thoughts regarding Ruby on Rails?", creator: jane)
+topic2 = Topic.create(name: "Colorado Lakes", description: "Does Colordo have any natrual lakes?", creator: jimmy)
