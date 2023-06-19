e0ff2bd68a6b5dc49459e437b1646909232c5c4b
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-19 16:18:13 -0600

    Created Errors.js and a css to handle error messages.

---
+++ client/src/errors/Errors.js
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import { ErrorsContext } from "../context/ErrorsContext";
+
+const Errors = () => {
+    const { errors } = useContext(ErrorsContext);
+    const errorList = errors.map((error, idx) => <p key={idx}>{ error }</p>)
+
+    return (
+        <p className="errors">{ errorList }</p>
+    )
+}
+
+export default Errors;
