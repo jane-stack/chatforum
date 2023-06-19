25eaf7ad6079385b55b6dc0fb993b65b3c31750c
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-19 15:27:22 -0600

    chat controller successful with index, create, update, and destroy

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,11 @@
 class ChatsController < ApplicationController
     before_action :find_topic
+    before_action :find_chat, only: [:update, :destroy]
     skip_before_action :authorize, only: [:index]
+    before_action :unprocessable_entity_if_not_found, only: [:update, :destroy]
+    before_action only: [:update, :destroy] do
+        authorize_user_resource(@chat.user_id)
+    end
 
     def index
         @topic = Topic.find(params[:topic_id])
@@ -19,7 +24,13 @@ class ChatsController < ApplicationController
     end
 
     def update
+        @chat.update(chat_params)
+        render json: @chat
+    end
 
+    def destroy
+        @chat.destroy
+        render json: { message: "Content Destroyed" }
     end
 
     private
@@ -30,6 +41,14 @@ class ChatsController < ApplicationController
     
     def find_topic
         @topic = Topic.find(params[:topic_id])
+    end
+
+    def find_chat
+        @chat = Chat.find_by_id(params[:id])
+    end
+
+    def unprocessable_entity_if_not_found
+        render json: { message: "Content not found" }, status: :unprocessable_entity unless @chat
     end
 
 end
