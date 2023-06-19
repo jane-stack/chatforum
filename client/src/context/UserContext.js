3a2d1d0ed2fca12cc1641bdf10d7e55dc51cb920
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-19 16:35:34 -0600

    created UserContext

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -4,18 +4,39 @@ const UserContext = createContext({});
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState([]);
-    const [currentUser, setCurrentUser] = useState(null);
     const [loggedIn, setLoggedIn] = useState(false);
 
     useEffect(() => {
         fetch('/me')
-        .then(resp => resp.json())
-        .then(data => {
-            if (!data.errors) {
-                loggedIn(data)
-            } else {
-                
+        .then(resp => {
+            if (resp.ok) {
+                resp.json().then(data => {
+                    setUser(data)
+                    data.error ? setLoggedIn(false) : setLoggedIn(true)
+                })
             }
         })
-    })
+    }, [])
+
+    const login = (user) => {
+        setUser(user)
+        setLoggedIn(true)
+    }
+
+    const logout = () => {
+        setUser({})
+        setLoggedIn(false)
+    }
+
+    const signup = (user) => {
+        setUser(user)
+        setLoggedIn(true)
+    }
+
+    return (
+        <UserContext.Provider value={{ user, login, logout, signup, loggedIn }}>{ children }</UserContext.Provider>
+    )
+
 }
+
+export { UserContext, UserProvider };
