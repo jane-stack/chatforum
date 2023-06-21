84704c2440d9e37311b3db57d99fb7c10b2797c1
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-21 16:56:10 -0600

    updated home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,16 @@
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
 function Home() {
+    const { user } = useContext(UserContext);
+
     return (
         <div>
-            <h3 className="App">Welcome to my ChatForum Project!</h3>
+            <h3 className="App">Welcome { user.username }!</h3>
+            <h5>You are visiting Tuyen's phase four ChatSpace project.</h5>
+            <div className="home-page">
+            <p>In this application, you can sign up with your own username and password or signin if you've already joined ChatSpace. As a user, you are able to post a question up for discussion. Other users can comment on your topic and vice-versa!</p>     
+            </div>
         </div>
     )
 }
