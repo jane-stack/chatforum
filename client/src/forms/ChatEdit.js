f1e1afdb1069402d0c86cb184d4350c9e7f4d32c
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-21 14:41:10 -0600

    ChatEdit.js form filled out

--- client/src/forms/ChatEdit.js
+++ client/src/forms/ChatEdit.js
@@ -1,6 +1,34 @@
+import { useContext, useState } from "react";
+import { ErrorsContext } from "../context/ErrorsContext";
+
 function ChatEdit() {
+    const { setErrors } = useContext(ErrorsContext);
+    const [content, setContent] = useState(chat.content);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const editComment = {
+            content: content
+        }
+        fetch(`/topics/${topic.id}/chats/${chat.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(editComment)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            editChat(data)
+            setEditMode(!editMode);
+        })
+    }
+
     return (
-        <form></form>
+        <form className="post-form" onSubmit={handleSubmit}>
+            <div className="new-post">
+            <textarea className="chat-textarea" type="text" name="content" placeholder="Write your comment." value={content} onChange={(e) => setContent(e.target.value)} /><br/>
+            <button type="submit">SEND</button>
+            </div>
+        </form>
     )
 }
 
